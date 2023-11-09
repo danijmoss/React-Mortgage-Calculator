e624342c7b7af12095bcaf41aa34479627eb7d38
Danielle Moss <dani.code.camp@gmail.com>
2023-11-09 17:56:38 -0500

    created template for the slider

--- src/components/common/SliderComponent.jsx
+++ src/components/common/SliderComponent.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
+import { Stack, Typography } from "@mui/material";
 
-const SliderComponent = ({ defaultValue, min, max }) => {
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
   return (
-    <Slider
-      defaultValue={defaultValue}
-      min={min}
-      max={max}
-      aria-label="Default"
-      valueLabelDisplay="auto"
-    />
+    <>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">Home Value</Typography>
+        <Typography variant="h5">$ 3000</Typography>
+      </Stack>
+      <Slider
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+        value={value}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          $1000
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          $10000
+        </Typography>
+      </Stack>
+      {/* Start video at 1:26:54 */}
+    </>
   );
 };
 
