10b5085d94e2500bc723c2e5b001f3c18c5e75c9
Danielle Moss <dani.code.camp@gmail.com>
2023-11-09 11:16:57 -0500

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Navbar from "./components/Navbar";
+import Result from "./components/Result";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
 
 function App() {
   return (
     <div className="App">
-      Hello everyone!
+      <Navbar/>
+      <SliderSelect/>
+      <TenureSelect/>
+      <Result/>
     </div>
   )
 }
